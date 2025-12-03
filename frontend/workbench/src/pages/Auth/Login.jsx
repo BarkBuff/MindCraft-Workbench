29c827e7d4daf19bfe770520fd417ec0d967171f
barkbuff <connect.arindamhore@gmail.com>
2025-12-04 01:03:29 +0530

    Update test changes to initialize setupCode

--- frontend/workbench/src/pages/Auth/Login.jsx
+++ frontend/workbench/src/pages/Auth/Login.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const App = () => {
+const Login = () => {
   return (
     <div className="text-5xl text-orange-600">SandBox</div>
   )
 }
 
-export default App
+export default Login
--- frontend/workbench/src/pages/Auth/SignUp.jsx
+++ frontend/workbench/src/pages/Auth/SignUp.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const App = () => {
+const SignUp = () => {
   return (
     <div className="text-5xl text-orange-600">SandBox</div>
   )
 }
 
-export default App
+export default SignUp
