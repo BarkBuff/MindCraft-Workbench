bfab442b5784aa2d2383249f28c2cd8abaab6df6
Harsh Vardhan <harshvardhan012006@gmail.com>
2025-12-04 10:59:40 +0530

    Pushing sample changes to codeBase

--- frontend/workbench/src/pages/Auth/Login.jsx
+++ frontend/workbench/src/pages/Auth/Login.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="text-5xl text-orange-600">SandBox</div>
+    <> 
+      <div className="text-5xl text-orange-600">SandBox</div>
+      <div className="text-5xl text-orange-600">Sand</div>
+    </>
+    
   )
 }
 
