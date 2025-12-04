f5cb5a94490c20faccdf3088d13eff2cffbbbdb2
barkbuff <connect.arindamhore@gmail.com>
2025-12-04 12:44:29 +0530

    Update with the Routing stuff to design the coreMapping

--- frontend/workbench/src/App.jsx
+++ frontend/workbench/src/App.jsx
@@ -1,11 +1,38 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
+import Login from "./pages/Auth/Login.jsx";
+import SignUp from "./pages/Auth/SignUp.jsx";
+import LandingPage from "./pages/LandingPage.jsx";
+import Dashboard from "./pages/Home/Dashboard.jsx";
+import MindCraft from "./pages/MindCraft/MindCraft.jsx";
 
 const App = () => {
   return (
     <div>
-      SandBox
-    </div>
-  )
-}
+      <Router>
+        <Routes>
+          {/* Default Route */}
+          <Route path="/" element={<LandingPage />} />
 
-export default App
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/mindcraft/:sessionId" element={<MindCraft />} />
+        </Routes>
+      </Router>
+
+      <Toaster
+        toastOptions={{
+          className: "",
+          style: {
+            fontSize: "13px",
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ frontend/workbench/src/pages/MindCraft/MindCraft.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MindCraft = () => {
+  return (
+    <div>
+      MindCraft
+    </div>
+  )
+}
+
+export default MindCraft
